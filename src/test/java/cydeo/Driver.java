2e36f5759b24d60dfb65070510ed3d781775d47d
Mahir Mammedov <mahireli@gmail.com>
2022-07-16 09:41:34 -0500

    trigger practice

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -34,7 +34,7 @@ public class Driver {
             switch (browser) {
                 case "remote-chrome":
                     try {
-                        // assign your grid server address
+                        // assign your grid server addressetwe
                         String gridAddress = "52.90.101.317";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
