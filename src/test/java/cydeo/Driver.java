a71a6894b95f39ca4b15804b82755245c3867312
Mahir Mammedov <mahireli@gmail.com>
2022-07-17 10:19:18 -0500

    ip addrress4

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server addreswq
-                        String gridAddress = "107.22.80.230/";
+                        String gridAddress = "107.22.80.230";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
